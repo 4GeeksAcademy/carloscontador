c740df1078355f0e340320ade0bfce34bd9e2d20
Carg21 <carlosgv512@hotmail.com>
2024-12-18 15:15:11 +0000

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,19 @@ import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Contador from "./component/Contador.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
+setInterval(() => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+    
+    root.render(
+        <Contador digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
